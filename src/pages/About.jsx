170262e2f1bd4576a93c5aca67021cb072ce6488
Pratik Jagtap <107701703+pratikjagtap013@users.noreply.github.com>
2025-10-14 23:55:30 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,12 @@ function About() {
   return (
     <div className="about-page">
       <h1>About Us</h1>
-      <p>About Us — Our team of talented architects, designers, and consultants is dedicated to transforming creative visions into architectural masterpieces. Each member brings expertise, passion, and a commitment to excellence, ensuring that every project reflects our core values of quality, sustainability, and client-focused design.</p>
-      <p>We focus on clean, responsive design with an easy-to-navigate layout.</p>
+      <h2>BHOITE EPS TECH INNOVATIONS LLP. </h2>
+      <h3>Your safety, Our Priority.</h3>
+      <p> BHOITE EPS TECH INNOVATIONS LLP, established in 2025, is a forward-thinking manufacturing company specializing in flame-retardant Expanded Polystyrene (EPS) panels. We are committed to providing innovative, safe, and energy-efficient EPS materials that meet the growing demand for sustainable and fire-resistant Construction and Packaging industries. </p>
+      <p> Our EPS panels are engineered for use in a wide range of applications, including prefabricated structures, cold storage, insulation systems, modular buildings, sensitive equipment, electronics, industrial components, medical supplies, and temperature-sensitive goods. Designed with safety and durability in mind, our products comply with fire safety norms and help reduce energy costs.</p>
+      <p>At BHOITE EPS TECH INNOVATIONS LLP, our mission is to redefine safety, sustainability, and efficiency across industries through advanced EPS technology. We combine modern manufacturing techniques with strict quality control to deliver tailor-made solutions that meet the specific needs of our clients.</p>
+      <p>With a vision to become a trusted leader in fire-safe EPS manufacturing, we aim to expand our footprint across India and beyond delivering reliable products, sustainable solutions to the construction and packaging industry.</p>
     </div>
   );
 }
